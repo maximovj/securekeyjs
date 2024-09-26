b75fe2e69e6fbd74c7a5d69c1fed5fdc2f095642
Víctor J <maximovj@outlook.com>
2024-09-26 12:39:00 -0600

    * Se modifica NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,20 +2,19 @@ import routes from '../routes/routes.js';
 
 const NavBar = () => {
     return (<nav>
-        {/* Center Title */}
         <h1 className="text-2xl font-bold text-green-400">SecureKeyJS</h1>
         <ul className="flex space-x-4">
             <li>
-                <a href={routes.Root} className="hover:text-green-400">Home</a>
+                <a href={routes.Root} className="hover:text-green-400">Inicio</a>
             </li>
             <li>
                 <a href={routes.PasswordByLength} className="hover:text-green-400">Por Longitud</a>
             </li>
             <li>
-                <a href={routes.PasswordByType} className="hover:text-green-400">Por tipo</a>
+                <a href={routes.PasswordByType} className="hover:text-green-400">Por Tipo</a>
             </li>
             <li>
-                <a href={routes.PasswordFromText} className="hover:text-green-400">Por texto</a>
+                <a href={routes.PasswordFromText} className="hover:text-green-400">Por Texto</a>
             </li>
             <li>
                 <a href={routes.PasswordDevTool} className="hover:text-green-400">Para DevTools</a>
