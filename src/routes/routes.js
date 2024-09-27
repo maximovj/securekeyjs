06c8a4ee0a54d91923a4215594299d96c0715447
Víctor J <maximovj@outlook.com>
2024-09-26 22:51:38 -0600

    * Se actualiza workflow #13

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,9 +4,9 @@ console.log('baseUrl => ', baseUrl);
 const routes = {
     Root: `${baseUrl}`,
     PasswordByLength: `${baseUrl}password-by-length`,
-    PasswordByType: '/password-by-type',
-    PasswordFromText: '/password-from-text',
-    PasswordDevTool: '/password-dev-tool',
+    PasswordByType: `${baseUrl}password-by-type`,
+    PasswordFromText: `${baseUrl}/password-from-text`,
+    PasswordDevTool: `${baseUrl}/password-dev-tool`,
 };
 
 export default routes;
