f794159811febeebee0f8e8ce837a174aa68463d
Víctor J <maximovj@outlook.com>
2024-09-26 22:59:38 -0600

    * Se actualiza workflow #16

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,8 @@
+const baseUrl = import.meta.env.BASE_URL || '/';
+console.log('baseUrl => ', baseUrl);
+
 const routes = {
+    BaseName: `${baseUrl}`,
     Root: '/',
     PasswordByLength: '/password-by-length',
     PasswordByType: '/password-by-type',
