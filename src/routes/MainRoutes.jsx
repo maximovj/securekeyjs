544ac709927b40babf6ba8f0675c6293f732783f
Víctor J <maximovj@outlook.com>
2024-09-26 23:11:13 -0600

    * Se actualiza workflow #20

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -13,7 +13,7 @@ import 'react-toastify/dist/ReactToastify.min.css';
 
 const MainRoutes = () => {
     return (<>
-        <BrowserRouter basename={routes.BaseName}>
+        <BrowserRouter basename={'/'}>
             <ToastContainer />
             <Routes>
                 <Route path={routes.Root} element={<Layout />} >
